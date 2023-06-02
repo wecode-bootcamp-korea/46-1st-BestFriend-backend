8e589566b188f2f0862e1a1907dbafdf689c81c8
HaeJungg <gpwnd0120@gmail.com>
2023-06-02 11:33:03 +0900

    ADD: 장바구니 생성하기

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const productController = require("./productController");
 const userController = require("./userController");
+const cartController = require("./cartController");
 
 module.exports = {
   userController,
   productController,
+  cartController,
 };
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const productRouter = require("./productRouter");
 const userRouter = require("./userRouter");
+const cartRouter = require("./cartRouter");
 
 router.use("/products", productRouter.router);
 router.use("/users", userRouter.router);
+router.use("/carts", cartRouter.router);
 
 module.exports = router;

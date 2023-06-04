aa7271540e09e0191de93e79cd31f8d5062de60a
HaeJungg <gpwnd0120@gmail.com>
2023-06-04 16:34:45 +0900

    ADD: 위시리스트 table 생성, 기능 구현

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const productController = require("./productController");
 const userController = require("./userController");
 const cartController = require("./cartController");
+const likeController = require("./likeController");
 
 module.exports = {
   userController,
   productController,
   cartController,
+  likeController,
 };
---
+++ models/likeDao.js
@@ -0,0 +1,68 @@
+const appDataSource = require("./dataSource");
+
+const createLike = async (userId, productId) => {
+  try {
+    const result = await appDataSource.query(
+      `
+      INSERT INTO likes(
+        user_id,
+        product_id
+      ) VALUES (?, ?)
+      `,
+      [userId, productId]
+    );
+    return result;
+  } catch {
+    const error = new Error("dataSource Error");
+    error.statusCode = 400;
+
+    throw error;
+  }
+};
+
+const deleteLike = async (userId, productId) => {
+  try {
+    const result = await appDataSource.query(
+      `
+    DELETE FROM likes
+    WHERE user_id = ? AND product_id = ?
+    `,
+      [userId, productId]
+    );
+    return result;
+  } catch (err) {
+    const error = new Error("dataSource Error");
+    error.statusCode = 400;
+
+    throw error;
+  }
+};
+
+const getLike = async (userId) => {
+  try {
+    const result = await appDataSource.query(
+      `
+    SELECT
+      products.name,
+      products.image_url,
+      products.price
+    FROM likes
+    INNER JOIN products ON products.id = likes.product_id
+    WHERE likes.user_id = ?
+      `,
+      [userId]
+    );
+    return result;
+  } catch (err) {
+    const error = new Error("dataSource Error");
+    error.statusCode = 400;
+
+    throw error;
+  }
+};
+
+module.exports = {
+  createLike,
+  deleteLike,
+  getLike,
+};

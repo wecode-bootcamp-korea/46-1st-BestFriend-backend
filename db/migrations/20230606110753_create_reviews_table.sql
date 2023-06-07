d91703c241cc5e8ce0f98a345e462aa3b8bbcc59
HaeJungg <gpwnd0120@gmail.com>
2023-06-07 16:03:12 +0900

    migration 수정

--- db/migrations/20230606110753_create_reviews_table.sql
+++ db/migrations/20230606110753_create_reviews_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE reviews(
   user_id INT NOT NULL,
   product_id INT NOT NULL,
   comment VARCHAR(200) NULL,
-  ALTER TABLE reviews ADD created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
   CONSTRAINT reviews_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id),
   CONSTRAINT reviews_product_id_fkey FOREIGN KEY (product_id) REFERENCES products(id)

a56461ea17c54d1a27acb10784abb968bd371e5c
HaeJungg <gpwnd0120@gmail.com>
2023-06-07 14:55:21 +0900

    MODIFY: res값 changepoint 추가

--- services/orderService.js
+++ services/orderService.js
@@ -1,12 +1,19 @@
-const { orderDao } = require("../models");
+const { orderDao, userDao } = require("../models");
 const { v4 } = require("uuid");
 
 const createOrder = async (userId, totalPrice) => {
   const orderNumber = v4();
-  const orderId = await orderDao.createOrder(userId, orderNumber, totalPrice);
+  const { response } = await orderDao.createOrder(
+    userId,
+    orderNumber,
+    totalPrice
+  );
+
+  const { orderId, changePoint } = response;
 
   return {
     orderNumber,
+    changePoint,
   };
 };
 
